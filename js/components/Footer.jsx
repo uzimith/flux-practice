4d2bc63b8026cdc42ebb5d2bac99733eecb188b9
uzimith <uzimith.x9@gmail.com>
2015-02-02 00:54:31 +0900

    replace with watchify

--- js/components/Footer.jsx
+++ js/components/Footer.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
 
 export default React.createClass({
   render() {
     return(
         <div>
-          <Header />
         </div>
         );
   }
---
+++ js/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return(
+        <header>
+          <h1>todos</h1>
+        </header>
+        );
+  }
+});
+
