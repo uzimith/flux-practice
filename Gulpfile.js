df201d850f2de370852b7cd555fcdca9c8cd1b7e
uzimith <uzimith.x9@gmail.com>
2015-02-06 16:03:03 +0900

    add gulp

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+var browserify = require('browserify');
+
+gulp.task('watch', function() {
+  watchify.args.fullPaths = false;
+  var bundler = watchify(browserify('./js/app.coffee', watchify.args));
+
+  bundler.on('update', rebundle);
+
+  function rebundle() {
+    return bundler.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./js/'));
+  }
+
+  return rebundle();
+});
