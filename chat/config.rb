26883c649772468e31d073845f68f980cfff2df3
uzimith <uzimith.x9@gmail.com>
2015-02-11 21:53:39 +0900

    fix css config

--- chat/config.rb
+++ chat/config.rb
@@ -10,13 +10,15 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :nested
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
+
+sourcemap = true
 
 
 # If you prefer the indented syntax, you might want to regenerate this
